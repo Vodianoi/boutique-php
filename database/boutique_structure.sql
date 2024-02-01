8bfe4ae14964b872ee1c4206b643e48497b40733
Axel Poty <axel.poty@le-campus-numerique.fr>
2024-02-01 12:14:00 +0100

    update sql dumps

--- database/boutique_structure.sql
+++ database/boutique_structure.sql
@@ -13,55 +13,60 @@ USE `boutique`;
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories`
 (
-    `id`   int(11) unsigned NOT NULL,
-    `name` varchar(45) DEFAULT NULL,
+    `id`   int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(45)      NOT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `id_UNIQUE` (`id`)
+    UNIQUE KEY `id_UNIQUE` (`id`),
+    UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE = InnoDB
+  AUTO_INCREMENT = 6
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_general_ci;
 
 
-DROP TABLE IF EXISTS `clients`;
-CREATE TABLE `clients`
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE `customers`
 (
-    `id`        int(11) unsigned NOT NULL,
-    `name`      varchar(45) DEFAULT NULL,
+    `id`        int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `lastname`  varchar(45)      NOT NULL,
     `firstname` varchar(45) DEFAULT NULL,
-    `email`     varchar(45) DEFAULT NULL,
-    `password`  varchar(45) DEFAULT NULL,
+    `email`     varchar(45)      NOT NULL,
+    `password`  varchar(45)      NOT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `id_UNIQUE` (`id`)
+    UNIQUE KEY `id_UNIQUE` (`id`),
+    UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE = InnoDB
+  AUTO_INCREMENT = 6
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_general_ci;
 
 
-DROP TABLE IF EXISTS `commands`;
-CREATE TABLE `commands`
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`
 (
-    `id`            int(11) unsigned NOT NULL,
+    `id`            int(11) unsigned NOT NULL AUTO_INCREMENT,
     `command_date`  date DEFAULT NULL,
     `delivery_date` date DEFAULT NULL,
     `clients_id`    int(10) unsigned NOT NULL,
     PRIMARY KEY (`id`),
-    KEY `fk_commands_1_idx` (`clients_id`),
-    CONSTRAINT `fk_commands_clients_1` FOREIGN KEY (`clients_id`) REFERENCES `clients` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    KEY `fk_orders_customers_1_idx` (`clients_id`),
+    CONSTRAINT `fk_orders_customers_1` FOREIGN KEY (`clients_id`) REFERENCES `customers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB
+  AUTO_INCREMENT = 6
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_general_ci;
 
 
-DROP TABLE IF EXISTS `commands_products`;
-CREATE TABLE `commands_products`
+DROP TABLE IF EXISTS `orders_products`;
+CREATE TABLE `orders_products`
 (
-    `commands_id` int(10) unsigned NOT NULL,
+    `orders_id`   int(10) unsigned NOT NULL,
     `products_id` int(10) unsigned NOT NULL,
-    `quantity`    int(11) DEFAULT NULL,
-    PRIMARY KEY (`commands_id`, `products_id`),
+    `quantity`    int(11)          NOT NULL,
+    PRIMARY KEY (`orders_id`, `products_id`),
     KEY `fk_commands_products_1_idx` (`products_id`),
-    CONSTRAINT `commands_products_ibfk_1` FOREIGN KEY (`commands_id`) REFERENCES `commands` (`id`),
-    CONSTRAINT `commands_products_ibfk_2` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+    CONSTRAINT `orders_products_ibfk_1` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`),
+    CONSTRAINT `orders_products_ibfk_2` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_general_ci;
@@ -70,20 +75,22 @@ CREATE TABLE `commands_products`
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products`
 (
-    `id`            int(11) unsigned NOT NULL,
+    `id`            int(11) unsigned NOT NULL AUTO_INCREMENT,
     `title`         varchar(45)      NOT NULL,
     `description`   tinytext                  DEFAULT NULL,
     `price`         decimal(10, 2)   NOT NULL,
     `weight`        decimal(10, 2)            DEFAULT NULL,
     `tax`           decimal(10, 2)            DEFAULT NULL,
+    `ttc`           decimal(10, 2)            DEFAULT NULL,
     `stock`         decimal(10, 2)   NOT NULL DEFAULT 0.00,
     `categories_id` int(10) unsigned          DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `fk_products_categories_1_idx` (`categories_id`),
     CONSTRAINT `fk_products_categories_1` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB
+  AUTO_INCREMENT = 6
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_general_ci;
 
 
--- 2024-02-01 10:45:32
+-- 2024-02-01 11:09:40
