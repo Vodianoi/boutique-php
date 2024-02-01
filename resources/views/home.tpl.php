6454e5dd81f4adb0024b777b5d0c655caffb440c
Axel Poty <axel.poty@le-campus-numerique.fr>
2024-02-01 15:35:40 +0100

    home page display

--- resources/views/home.tpl.php
+++ resources/views/home.tpl.php
@@ -1,8 +1,16 @@
-<h1>Latest products</h1>
-<?php foreach ($products as $product): ?>
-    <div class="blog-post">
-        <a href="?action=product&id=<?= $product['id'] ?>"><h2><?= $product['title']; ?></h2></a>
-        <p><strong>Price:</strong> <?= $product['ttc']; ?></p>
-    </div>
-<?php endforeach ?>
+<div class="container">
+    <h1>Latest Products</h1>
 
+    <div class="product-container">
+        <?php foreach ($products as $product): ?>
+            <div class="product-item">
+                <img src="https://placehold.co/100x100/" alt="Product Image" class="product-image">
+                <a href="?action=product&id=<?= $product['id'] ?>">
+                    <h2><?= $product['title']; ?></h2>
+                </a>
+                <p><strong>Price:</strong> <?= $product['ttc']; ?></p>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        <?php endforeach ?>
+    </div>
+</div>
