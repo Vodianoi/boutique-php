9d08a596a21b24a9491d97b69255a79210c61be4
Axel Poty <axel.poty@le-campus-numerique.fr>
2024-02-02 11:14:54 +0100

    fix: single product page display

--- resources/views/product/show.php
+++ resources/views/product/show.php
@@ -1,14 +1,17 @@
-<div class="product-container">
+<div class="product-container" style="flex-direction: row-reverse; max-width: 1000px">
     <h2><?= $product['title'] ?></h2>
-    <img src="https://placehold.co/200x200/" alt="Product Image" class="product-image">
-    <div class="product-details">
-        <p><?= $product['description'] ?></p>
-        <p class="price"><?= $product['ttc'] ?>€</p>
-        <form action="?action=cart" method="post">
-            <label> Quantity
-                <input type="number" name="quantity" value="1"/>
-            </label>
-            <button type="submit" class="add-to-cart">Add to Cart</button>
-        </form>
+    <div class="product-details" style="align-self: flex-end;">
+        <img src="https://placehold.co/200x200/" alt="Product Image" class="product-image">
+        <div style="float: right">
+            <p><?= $product['description'] ?></p>
+            <p class="price"><?= $product['ttc'] ?>€</p>
+            <form action="?action=cart" method="post">
+                <label> Quantity
+                    <input type="number" name="quantity" value="1"/>
+                </label>
+                <input id="id" name="id" type="hidden" value="<?= $product['id'] ?>"/>
+                <button type="submit" class="add-to-cart">Add to Cart</button>
+            </form>
+        </div>
     </div>
 </div>
