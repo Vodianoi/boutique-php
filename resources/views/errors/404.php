94e41c1be925e92407a2ace124664ec7e6b5d5af
Axel Poty <axel.poty@le-campus-numerique.fr>
2024-02-05 16:41:31 +0100

    feat: 404 page

--- resources/views/errors/404.php
+++ resources/views/errors/404.php
@@ -1,2 +1,6 @@
-<?php
-echo '404';
+<div class="container error-container">
+    <img class="error-image" src="https://placekitten.com/800/400" alt="404 Cat Image">
+    <h1 class="error-heading">404</h1>
+    <p class="error-message">Oops! The page you are looking for might be in another galaxy.</p>
+    <p><a class="error-link" href="/">Go back home</a></p>
+</div>
