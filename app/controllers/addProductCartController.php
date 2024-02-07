e661a3ffd94d745fad17f1b8b73e6f527e9d0aa5
PhilémonMaly <philemon.maly@le-campus-nemrique.fr>
2024-02-07 14:18:41 +0100

    step 3.2 à finir

---
+++ app/controllers/addProductCartController.php
@@ -0,0 +1,16 @@
+<?php
+
+require '../app/persistences/cart.php';
+require '../app/persistences/product.php';
+require '../app/controllers/showCartController.php';
+
+
+
+//if (isset($_SESSION['cart'])){
+//    $_SESSION['cart'] = [
+//        $productID => $quantity
+//        ];
+//}
+//
+//addProductCart($productID, $quantity);
+
